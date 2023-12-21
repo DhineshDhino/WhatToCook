6dfb429f5b335c0ab7e3f84cf1ff1e339fcf60c7
DhineshDhino <k.dhineshasan@gmail.com>
2023-12-21 12:02:36 +0530

    [not working] - added integration Tests for userController class but not working as expected

---
+++ src/test/java/com/recipe/WhatToCook/integrationTests/UserControllerIT.java
@@ -0,0 +1,35 @@
+package com.recipe.WhatToCook.integrationTests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class UserControllerIT {
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setUp() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(webApplicationContext)
+                .build();
+    }
+    @Test
+    public void getUser() throws Exception {
+        this.mockMvc.perform(get("/allUser"))
+                .andExpect(status().isOk());
+    }
+}
